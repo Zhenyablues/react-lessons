0232602b1ecabe62ab67d8a75bc673e4e50edbc4
Zhenyablues <66002467+Zhenyablues@users.noreply.github.com>
2023-03-20 12:08:20 +0200

    Add files via upload

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Yevhen</h1>
+      <h3>I am study on react course</h3>
+      <button>Like</button>
+    </React.Fragment>
+  );
+};
+
+export default Card;
