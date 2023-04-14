30d20533925ae21107aa601ebc8856d35c8254e4
Zhenyablues <66002467+Zhenyablues@users.noreply.github.com>
2023-04-14 11:30:00 +0300

    Add files via upload

---
+++ 26-react-router/src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+  {
+    title: 'Complite React Guide',
+    slug: 'react',
+    id: 257,
+  },
+  {
+    title: 'Complite JavaScript Guide',
+    slug: 'javascript',
+    id: 573,
+  },
+  {
+    title: 'Complite Python Guide',
+    slug: 'python',
+    id: 872,
+  },
+  {
+    title: 'Complite Node.js Guide',
+    slug: 'node-js',
+    id: 361,
+  },
+];
+
+export default courses;
